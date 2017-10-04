aa7772837380198d3acedb168d50126076c07fae
YukiYamaji <voc01.miku39@gmail.com>
2017-10-04 12:46:48 +0900

    UI差し替えテスト

---
+++ Assets/Scripts/Yamazi/MoneyUI.cs
@@ -0,0 +1,71 @@
+﻿/////////////////
+//制作  山路優樹
+//日付  10月2日
+//説明  所持金UI処理
+/////////////////
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoneyUI : MonoBehaviour
+{
+    int showValue;                              //表示値
+    float width;                                //一桁横幅
+    public Sprite[] imageNum;                   //各数値画像配列
+    [SerializeField]
+    GameObject showSprite;                      //画像表示用オブジェ
+    GameObject[] numPos;                        //表示用オブジェ配列
+    public Dictionary<char, Sprite> sprite;     //ディクショナリ
+
+    void Start()
+    {
+        sprite = new Dictionary<char, Sprite>()
+            {
+                { '0', imageNum[0] },
+                { '1', imageNum[1] },
+                { '2', imageNum[2] },
+                { '3', imageNum[3] },
+                { '4', imageNum[4] },
+                { '5', imageNum[5] },
+                { '6', imageNum[6] },
+                { '7', imageNum[7] },
+                { '8', imageNum[8] },
+                { '9', imageNum[9] },
+            };
+    }
+
+    // 表示する値
+    public int Value
+    {
+        get
+        {
+            return showValue;
+        }
+        set
+        {
+            showValue = value;
+            // 表示文字列取得
+            string strValue = value.ToString();
+            // 現在表示中のオブジェクト削除
+            if (numPos != null)
+            {
+                foreach (GameObject numSprite in numPos)
+                {
+                    GameObject.Destroy(numSprite);
+                }
+            }
+            // 表示桁数分だけオブジェクト作成
+            numPos = new GameObject[strValue.Length];
+            for (int i = 0; i < numPos.Length; ++i)
+            {
+                // オブジェクト作成
+                numPos[i] = Instantiate(showSprite, transform.position + new Vector3((float)i * width, 0), Quaternion.identity) as GameObject;
+                // 表示する数値指定
+                numPos[i].GetComponent<SpriteRenderer>().sprite = sprite[strValue[i]];
+                // 自身の子階層に移動
+                numPos[i].transform.parent = transform;
+            }
+        }
+    }
+}
--- Assets/Scripts/Yamazi/UI.cs
+++ Assets/Scripts/Yamazi/UI.cs
@@ -4,6 +4,7 @@
 //説明  UI処理
 /////////////////
 
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -17,12 +18,13 @@ namespace Yamaji
         int hp;                             //現在体力
         int maxHp = 100;                    //最大体力
         int displayHp;                      //比較用体力
+        int damageHp;                       //比較用ダメージ
         int money;                          //現在所持金
         int maxMoney = 9999;                //最大所持金
-        public Image hpImage;               //HPゲージ
-        public Text moneyText;              //所持金
+        public Image imageHp;               //HP
+        public Image imageDamage;           //ダメージ
         public UIManager uiManagerScript;   //UIマネージャー取得
-
+        
         //初期化処理
         void Start()
         {
@@ -30,6 +32,7 @@ namespace Yamaji
             money = uiManagerScript.GetMoneyValue();
             hp = uiManagerScript.GetHpValue();
             displayHp = hp;
+            damageHp = maxHp - hp;
             Money();
             HpGauge(displayHp);
         }
@@ -39,20 +42,20 @@ namespace Yamaji
         {
             //現在体力と比較用体力を比べて、異なっていれば徐々に減算
             if (displayHp != hp) displayHp = (int)Mathf.Lerp(displayHp, hp, 0.1f);
+            //if (damageHp != maxHp) damageHp = (int)Mathf.Lerp(damageHp, maxHp, 0.1f);
             //現在体力の割合により文字色変化
-            float HpPercentage = (float)displayHp / maxHp;
-            if (HpPercentage > 0.5f) hpImage.color = Color.green;
-            else if (HpPercentage > 0.3f) hpImage.color = Color.yellow;
-            else hpImage.color = Color.red;
+            float hpPercentage = (float)displayHp / maxHp * 2.5f;
+            //float damagePercentage = (float)damageHp / maxHp;
             //ゲージの長さを体力の割合により伸縮
-            hpImage.transform.localScale = new Vector3(HpPercentage, 1, 1);
+            imageHp.transform.localScale = new Vector3(0.5f, hpPercentage, 1);
+            //imageDamage.transform.localScale = new Vector3(1.0f, damagePercentage, 1);
         }
 
         //所持金テキスト管理
         void Money()
         {
-            //書式設定
-            moneyText.text = string.Format("{0:0,000}", money);
+            MoneyUI numSprite = GetComponent<MoneyUI>();
+            numSprite.Value = money; //ここで表示数値指定
             //所持金値制限
             money = (int)Mathf.Clamp(money, 0, maxMoney);
         }
@@ -60,7 +63,7 @@ namespace Yamaji
         //表示所持金セット
         public void SetMoneyText(int setValue)
         {
-            moneyText.text = setValue.ToString();
+            Debug.Log(money);
         }
     }
 }
