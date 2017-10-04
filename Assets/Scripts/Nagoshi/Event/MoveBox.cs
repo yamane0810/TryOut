cf3ec0239503dbfde7fd98b4823a66559d248931
YUUTO\大樹 <yakmsy.memories@ezweb.ne.jp>
2017-10-04 17:34:07 +0900

    MoveBoxのイベント完成

---
+++ Assets/Scripts/Nagoshi/Event/MoveBox.cs
@@ -0,0 +1,74 @@
+﻿////////////////////////////
+//製作者　名越大樹
+//製作日　10月4日
+//クラス名　MoveBoxの動きに関するクラス
+////////////////////////////
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Nagoshi
+{
+    public class MoveBox : MonoBehaviour
+    {
+        [SerializeField]
+        GameObject instancePos;
+        [SerializeField]
+        GameObject instanceMoveBox;
+        [SerializeField]
+        bool isSideMove;
+        [SerializeField]
+        bool isLengthMove;
+        [SerializeField]
+        float speed;
+        [SerializeField]
+        float time;
+        float copytime;
+        Vector3 movePos;
+        // Use this for initialization
+        void Start()
+        {
+            float length = 0;
+            float side = 0;
+            copytime = time;
+            if (isLengthMove)
+            {
+                length = 1;
+            }
+            if (isSideMove)
+            {
+                side = 1;
+            }
+            movePos = new Vector3(side * speed, length * speed, 0.0f);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Move();
+            Count();
+        }
+
+        void Move()
+        {
+            transform.Translate(movePos * Time.deltaTime);
+        }
+
+        void Count()
+        {
+            time -= Time.deltaTime;
+            if(time <= 0)
+            {
+                movePos = -movePos;
+                time = copytime;
+            }
+        }
+
+        public void Stop()
+        {
+            Instantiate(instanceMoveBox, instancePos.transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+}
