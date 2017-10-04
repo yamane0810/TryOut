849becbe90b455c4af27cb7a9c764d7c36831b81
yamane0810 <yamasan279@gmail.com>
2017-10-04 13:37:11 +0900

    ゴール後にリザルト画面へ移行

--- Assets/Scripts/Managers/CollisionManager.cs
+++ Assets/Scripts/Managers/CollisionManager.cs
@@ -6,18 +6,34 @@
 
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
-public class CollisionManager : MonoBehaviour {
+public class CollisionManager : MonoBehaviour
+{
     /// <summary>
     /// プレイやーがほかのオブジェクト衝突した時の処理
     /// </summary>
-    public void HitPlayer(GameObject playerobj,GameObject hitobj)
+    public void HitPlayer(GameObject playerobj, GameObject hitobj)
     {
+
         //宝箱(coin)がプレイヤーに当たった時を宝箱を消す
         if (hitobj.gameObject.tag == "coin")
-        {   
+        {
             Destroy(hitobj);
+
+            //イベントのオブジェクトに衝突した時
+            if (hitobj.tag == "Event")
+            {
+                playerobj.GetComponent<Nagoshi.PlayerStatus>().SetEventObj(hitobj);
+            }
+            //ゴールオブジェクトに衝突した時
+            else if (hitobj.tag == "Goal")
+            {
+                //ゴールした後リザルト画面へ移行
+                SceneManager.LoadScene("ResultScene");
+               // Debug.Log("ゴール！");
+            }
         }
     }
 }
