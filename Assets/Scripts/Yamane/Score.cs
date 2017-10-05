2fe076450d62a03d76f7907a6cd7c101ba655799
yamane0810 <yamasan279@gmail.com>
2017-10-05 20:06:03 +0900

    リザルトのスコア等

--- Assets/Scripts/Yamane/Score.cs
+++ Assets/Scripts/Yamane/Score.cs
@@ -15,27 +15,42 @@ public class Score : MonoBehaviour
     int hp;
     int score;
     int TotalScore = 0;
-    
+
+
     public Text scoretxt;
-   
+    public Text money;
+    public Text life;
+    public Text ts;
+
 
     void Start()
     {
-
         //テキストファイルの中を読み込み
         //読み込む際に最初にAssetsから読み込んでもらう   Application.dataPath  = (Assets) 
         StreamReader sr = new StreamReader(Application.dataPath + "/Resources/score.txt");
         // Debug.Log("Test！");
         //一行読み込み
         string TotalScore = sr.ReadToEnd();
-        scoretxt.text = TotalScore;
-
+        //テキストデータ内の　/　が区切り 3000/100/30000
+        string[] splitted = TotalScore.Split('/');
+        //お金
+        string score = splitted[0];
+        money.text = score;
+            //体力
+        string hp = splitted[1];
+        life.text = hp;
+        //トータルスコア
+        string tScore = splitted[2];
+        ts.text = tScore;
     }
-    void Update()
+        void Update()
     {
-        //合計得点（リザルト時の得点）体力　*　スコア　=　トータルスコア
-        TotalScore = hp * score;
-        scoretxt.text = "Score:" +  TotalScore;
-    }
+
+            //合計得点（リザルト時の得点）体力　*　スコア　=　トータルスコア
+           TotalScore = hp * score;
+           scoretxt.text = "" + TotalScore;
+
+        }
+    
 }
 
